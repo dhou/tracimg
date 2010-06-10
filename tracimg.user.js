3fa772e8865494cf26f5e215736d31362fec65c8
Damien Hou <i@damienh.org>
2010-06-10 14:04:19 +0800

    show swf links inline on page

--- tracimg.user.js
+++ tracimg.user.js
@@ -19,11 +19,15 @@ function renderImages() {
         for (var j = 0; j < content_links.length; j++) {
             link = content_links[j];
             src = link.href;
-            if (src.lastIndexOf('.png') != -1 || src.lastIndexOf('.gif') != -1 || src.lastIndexOf('.jpg') != -1) {
+            if (src.lastIndexOf('.png') > 0 || src.lastIndexOf('.gif') > 0 || src.lastIndexOf('.jpg') > 0) {
                 var image = document.createElement("div");
                 image.innerHTML = '<img style="max-width:620px;" src="' + src + '?format=raw" /><br />';
                 link.parentNode.insertBefore(image, link);
-            }
+            } else if (src.lastIndexOf('.swf') > 0) {
+				var swf = document.createElement('div');
+				swf.innerHTML = '<object type="application/x-shockwave-flash" width="400" height="300" data="' + src + '"><param name="wmode" value="opaque"></object>';
+				link.parentNode.insertBefore(swf, link);
+			}
         }
     }
 }
